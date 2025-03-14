ff4cfec34ea8db62b3fb56dc6156595702db5bf5
lady.program <nymeriastark74@gmail.com>
2025-03-14 11:15:54 +0100

    Second use of retrofit, to retrieve some heroes in an example call

---
+++ app/src/main/java/com/ladyprogram/superhero/activities/DetailActivity.kt
@@ -0,0 +1,9 @@
+package com.ladyprogram.superhero.activities
+
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import com.ladyprogram.superhero.R
+
+class DetailActivity : AppCompatActivity() {
+
+}
